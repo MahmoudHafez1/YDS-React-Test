081abe3d572d62834806430b8f59e2fe808398c5
MahmoudHafez1 <mmhafez51@gmail.com>
2022-07-02 09:52:18 +0200

    From and List integration

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import Form from "./components/Form";
 import List from "./components/List";
 import "./App.css";
 
 const App = () => {
+  const [formMode, setFormMode] = useState("new");
+  const [reloadAddressList, setReloadAddressList] = useState(false);
+
+  const formModeHandler = (addressId = "new") => {
+    setFormMode(addressId);
+  };
+
+  const reloadAddressListHandler = () => {
+    setReloadAddressList((state) => !state);
+  };
+
   return (
-    <div className='App'>
+    <div className="App">
       <h1>YDS - Task</h1>
 
-      <Form />
+      <Form
+        formMode={formMode}
+        formModeHandler={formModeHandler}
+        reloadAddressListHandler={reloadAddressListHandler}
+      />
 
-      <div className='divider' />
-      <List />
+      <div className="divider" />
+      <List
+        formModeHandler={formModeHandler}
+        reloadAddressList={reloadAddressList}
+      />
     </div>
   );
 };
